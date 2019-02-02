8cd48ca6bb6ae9e2bb5f1f1ee5731a29910020f8
gK <yankskwesi@gmail.com>
2019-02-02 15:21:40 -0500

    Intial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#README
+
+A test run of the usuage of sonic-pi with pyyython. Using the psonic package. As a part of livecode.nyc
+
+
---
+++ intro.py
@@ -0,0 +1,15 @@
+from psonic import *
+
+play(60)
+
+#use_bpm(120)
+pause = 1
+n = 1
+while n < 10:
+    play(60)
+    sleep(pause)
+    play(62)
+    play(65)
+    play(73)
+    n += 1
+
