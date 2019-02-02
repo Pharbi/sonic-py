c07864b1ea16b4d5cab0625c3af05bae84a870ad
gK <yankskwesi@gmail.com>
2019-02-02 17:13:48 -0500

    updated ignore file

--- intro.py
+++ intro.py
@@ -1,15 +1,30 @@
 from psonic import *
+from threading import Thread
 
 play(60)
 
 #use_bpm(120)
 pause = 1
 n = 1
-while n < 10:
-    play(60)
-    sleep(pause)
-    play(62)
+#while n < 10:
+#    play(60)
+#    sleep(pause)
+#    play(62)
+#    play(65)
+#    play(73)
+#    n += 1
+
+def loop():
     play(65)
-    play(73)
-    n += 1
+    sleep(1)
+    play(70)
+
+def looper():
+    while True:
+        loop()
+
+looper_thread = Thread(name='looper', target=looper)
+
+looper_thread.start()
+input("Press Enter to continue...")
 
